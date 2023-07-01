c44495ab2a192a1ef64162140c152efe45fea22b
Vinay Kumar <vinaykumar0199@gmail.com>
2023-07-01 14:38:40 +0530

    Day 8 complete

---
+++ lib/Models/catalog.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imgURL;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.imgURL});
+}
+
+final products = [
+  Item(
+      id: "abc001",
+      name: "iPhone 12 pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      imgURL:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+];
